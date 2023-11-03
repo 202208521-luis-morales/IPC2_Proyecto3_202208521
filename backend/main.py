8b8dcfb5f7ff2e45de0438b931ad56cd831e477c
202208521-luis-morales <3683798410101@ingenieria.usac.edu.gt>
2023-11-03 01:31:27 -0600

    Se corrigieron detalles

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request, Response
+from flask_cors import CORS
+import xml.etree.ElementTree as ET
+import re
+from operator import itemgetter
+from itertools import groupby
+from datetime import datetime
+
+import unidecode
 
 app = Flask(__name__)
+CORS(app)
 
 # ver dataStructure.js para ver como se distribuye la estructura
-data = {
+dbData = {
   "messages": [],
   "dictionary": {
     "positives": [],
@@ -15,14 +24,202 @@ data = {
 def reset():
     return "<p>Reset Works!</p>"
 
+@app.route("/getConfig")
+def get_messages():
+    root = ET.Element("CONFIG_RECIBIDA")
+    ET.SubElement(root, "PALABRAS_POSITIVAS").text = dbData["dictionary"]["positives"]
+    ET.SubElement(root, "PALABRAS_POSITIVAS_RECHAZADAS").text = "0"
+    ET.SubElement(root, "PALABRAS_NEGATIVAS").text = dbData["dictionary"]["negatives"]
+    ET.SubElement(root, "PALABRAS_NEGATIVAS_RECHAZADAS").text = "0"
+
+    xml_string = ET.tostring(root, encoding='utf8').decode('utf8')
+    return Response(xml_string, content_type='application/xml')
+
+@app.route("/getMessages")
+def get_messages():
+    # Generar la lista para insertarla en el .xml
+    dbData["messages"].sort(key=itemgetter('date'))
+
+    result = []
+    for date, group in groupby(dbData["messages"], key=itemgetter('date')):
+        message_group = list(group)
+        texts = [message['text'] for message in message_group]
+        result.append({'date': date, 'texts': texts})
+
+    print(result)
+
+    root = ET.Element("MENSAJES_RECIBIDOS")
+
+    for time in result:
+        tiempo = ET.SubElement(root, "TIEMPO")
+        texts = time["texts"]
+
+        ET.SubElement(tiempo, "FECHA").text = tiempo["date"]
+        ET.SubElement(tiempo, "MSJ_RECIBIDOS").text = len(texts)
+        ET.SubElement(tiempo, "USR_MENCIONADOS").text = encontrar_menciones(".".join(texts))
+        ET.SubElement(tiempo, "HASH_INCLUIDOS").text = encontrar_hashtags(".".join(texts))
+
+    xml_string = ET.tostring(root, encoding='utf8').decode('utf8')
+    return Response(xml_string, content_type='application/xml')
+
 @app.route("/loadMessages", methods=["POST"])
 def load_messages():
-    return "<p>Load Messages Works!</p>"
+    file = request.files['file']
+
+    if file and file.filename.endswith('.xml'):
+        xml_data = file.read()
+        root = ET.fromstring(xml_data)
+
+        for indx, mensaje in enumerate(root.findall("MENSAJE")):
+            patron_fecha = r'\d{2}/\d{2}/\d{4}'
+
+            match = re.search(patron_fecha, mensaje.find("FECHA").text)
+
+            if match:
+                fecha = match.group(0)
+                dbData["messages"].append({"date": fecha, "text": mensaje.find("TEXTO").text.strip()})
+
+                print(dbData)
+            else:
+                return f'No se encontró ninguna fecha dentro del mensaje {indx}', 400
+            
+        return 'Archivo XML cargado y procesado exitosamente', 200
+
+    return 'Sólo se aceptan archivos .xml', 400
 
 @app.route("/loadConfig", methods=["POST"])
 def load_config():
-    return "<p>Load Config Works!</p>"
+    file = request.files['file']
+
+    if file and file.filename.endswith('.xml'):
+        xml_data = file.read()
+        root = ET.fromstring(xml_data)
+
+        for palabra in root.find("sentimientos_positivos").findall("palabra"):
+            dbData["dictionary"]["positives"].append(palabra.text.strip())
+
+            print(dbData)
+
+        for palabra in root.find("sentimientos_negativos").findall("palabra"):
+            dbData["dictionary"]["negatives"].append(palabra.text.strip())
+
+            print(dbData)
+        
+        return 'Archivo XML cargado y procesado exitosamente', 200
+
+    return 'Sólo se aceptan archivos .xml', 400
 
 @app.route("/consult")
 def consult():
-    return "<p>Consult Works!</p>"
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    type_consult = request.args.get('type_consult')
+
+    # Generar la lista para insertarla en el .xml
+    dbData["messages"].sort(key=itemgetter('date'))
+
+    result = []
+    for date, group in groupby(dbData["messages"], key=itemgetter('date')):
+        message_group = list(group)
+        texts = [message['text'] for message in message_group]
+        result.append({'date': date, 'texts': texts})
+
+    print(result)
+
+    if type_consult == "hashtags":
+        return contar_hashtags(result, start_date, end_date), 200
+    elif type_consult == "mentions":
+        return contar_menciones_usuarios(result, start_date, end_date), 200
+    elif type_consult == "feelings":
+        return contar_sentimientos_palabras(result, start_date, end_date), 200
+    elif type_consult == "graphs":
+        return {"hashtags": contar_hashtags(result, start_date, end_date), "mentions": contar_menciones_usuarios(result), "feelings": contar_sentimientos_palabras(result, start_date, end_date)}, 200
+
+    return "Error: Ninguno de los tipos enviados concuerda con alguno de los tipos programados", 400
+
+def encontrar_menciones(texto):
+    # Utiliza una expresión regular para encontrar menciones de usuarios
+    menciones = re.findall(r'@[\wáéíóúÁÉÍÓÚüÜ]+', texto)
+    
+    # Normaliza las menciones para no distinguir mayúsculas y acentos
+    menciones_normalizadas = [unidecode(mencion.lower()) for mencion in menciones]
+    
+    # Utiliza un conjunto (set) para eliminar menciones duplicadas
+    menciones_unicas = set(menciones_normalizadas)
+    
+    return len(menciones_unicas)
+
+def encontrar_hashtags(texto):
+    hashtags = re.findall(r'#(.*?)#', texto)
+    
+    hashtags_normalizados = [unidecode(hashtag.lower()) for hashtag in hashtags]
+
+    hashtags_unicos = set(hashtags_normalizados)
+    
+    return len(hashtags_unicos)
+
+def contar_hashtags(lista_datos, start_date, end_date):
+    return [{"date": ld["date"], "list": _contar_hashtags(".".join(ld["texts"]))} for ld in lista_datos if datetime.strptime(start_date, "%Y-%m-%d") <= datetime.strptime(ld["date"], "%Y-%m-%d") <= datetime.strptime(end_date, "%Y-%m-%d")]
+
+def _contar_hashtags(texto):
+     # Utiliza una expresión regular para encontrar hashtags dentro de #...#
+    hashtags = re.findall(r'#(.*?)#', texto)
+    
+    # Normaliza los hashtags para no distinguir mayúsculas, minúsculas y acentos
+    hashtags_normalizados = [unidecode(hashtag.lower()) for hashtag in hashtags]
+    
+    # Cuenta la frecuencia de cada hashtag
+    conteo_hashtags = {}
+    for hashtag in hashtags_normalizados:
+        conteo_hashtags[hashtag] = conteo_hashtags.get(hashtag, 0) + 1
+    
+    return conteo_hashtags
+
+def contar_sentimientos_palabras(lista_datos, start_date, end_date):
+    return [{"date": ld["date"], "list": _contar_sentimientos_palabras(ld["texts"])} for ld in lista_datos if datetime.strptime(start_date, "%Y-%m-%d") <= datetime.strptime(ld["date"], "%Y-%m-%d") <= datetime.strptime(end_date, "%Y-%m-%d")]
+
+def _contar_sentimientos_palabras(lista_textos):
+    counter = {
+        "positives": 0,
+        "negatives": 0,
+        "neutral": 0
+    }
+
+    temp_counter = 0
+
+    for text in lista_textos:
+        for pos in dbData["dictionary"]["positives"]:
+            if pos in text:
+                temp_counter += 1
+        
+        for neg in dbData["dictionary"]["negatives"]:
+            if neg in text:
+                temp_counter -= 1
+
+        if temp_counter > 0:
+            counter["positives"] += 1
+        elif temp_counter < 0:
+            counter["negatives"] += 1
+        else:
+            counter["neutral"] += 1
+
+        temp_counter = 0
+    
+    return counter
+
+def contar_menciones_usuarios(lista_datos, start_date, end_date):
+    return [{"date": ld["date"], "list": _contar_menciones_usuarios(".".join(ld["texts"]))} for ld in lista_datos if datetime.strptime(start_date, "%Y-%m-%d") <= datetime.strptime(ld["date"], "%Y-%m-%d") <= datetime.strptime(end_date, "%Y-%m-%d")]
+
+def _contar_menciones_usuarios(texto):
+    # Utiliza una expresión regular para encontrar menciones de usuarios
+    menciones = re.findall(r'@[\wáéíóúÁÉÍÓÚüÜ]+', texto)
+    
+    # Normaliza las menciones para no distinguir mayúsculas, minúsculas y acentos
+    menciones_normalizadas = [unidecode(mencion.lower()) for mencion in menciones]
+    
+    # Cuenta la frecuencia de cada mención de usuario
+    conteo_menciones = {}
+    for mencion in menciones_normalizadas:
+        conteo_menciones[mencion] = conteo_menciones.get(mencion, 0) + 1
+    
+    return conteo_menciones
--- frontend/mainview/src/components/App.jsx
+++ frontend/mainview/src/components/App.jsx
@@ -1,35 +1,151 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Swal from "sweetalert2";
-import Chart from "chart.js/auto"
+import Chart from "chart.js/auto";
+import axios from 'axios';
+import pretty from 'pretty-format';
 
 export default function App() {
-  const chartRef = useRef(null);
-  function prueba() {
-    Swal.fire("Probando", "Probando texto", "success");
+  const chartRef1 = useRef(null);
+  const chartRef2 = useRef(null);
+  const chartRef3 = useRef(null);
+
+  const [typeConsult, setTypeConsult] = useState("hashtags");
+  const [textConsultPanel, setTextConsultPanel] = useState("Bienvenido: Realiza tu consulta");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [messagesFile, setMessagesFile] = useState(null);
+  const [configFile, setConfigFile] = useState(null);
+  const [dataForGraphs, setDataForGraphs] = useState({});
+
+  function processResponse(res, isError = false) {
+    console.log(res);
+    if (isError) {
+      Swal.fire("Error", res.response.data, "error");
+    } else {
+      Swal.fire("Éxito", res.data, "success");
+    }
+  }
+  const onMessageFileChange = (event) => {
+    event.preventDefault();
+    setMessagesFile(event.target.files[0]);
+  }
+
+  const onInfoStudentButtonClicked = (event) => {
+    Swal.fire({
+      title: "Información del estudiante",
+      html: '<div style="text-align: left; margin-left: 50px"><p>Nombre: Luis Rodrigo Morales Florián</p><p>Carnet: 202208521</p></div>',
+      icon: 'info',
+    });
+  }
+
+  const onConfigFileChange = (event) => {
+    event.preventDefault();
+    setConfigFile(event.target.files[0]);
+  }
+
+  const handleConfigFileUpload = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", configFile);
+
+    try {
+      let res = await axios.post('http://127.0.0.1:5000/loadConfig', formData);
+      processResponse(res);
+    } catch (error) {
+      processResponse(error, true);
+    }
+  }
+
+  const handleMessageGet = (event) => {
+    fetch('http://127.0.0.1:5000/getMessages')
+      .then(response => response.text())
+      .then(xml => {
+        const blob = new Blob([xml], { type: 'application/xml' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'resumen_mensajes.xml';
+        a.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch(error => console.error(error));
+  }
+
+  const handleConfigGet = (event) => {
+    fetch('http://127.0.0.1:5000/getConfig')
+      .then(response => response.text())
+      .then(xml => {
+        const blob = new Blob([xml], { type: 'application/xml' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'resumen_configuracion.xml';
+        a.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch(error => console.error(error));
+  }
+
+  const handleMessageFileUpload = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", messagesFile);
+
+    try {
+      let res = await axios.post('http://127.0.0.1:5000/loadMessages', formData);
+      processResponse(res);
+    } catch (error) {
+      processResponse(error, true);
+    }
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let res = await axios.get(`http://127.0.0.1:5000/consult?start_date=${startDate}&end_date=${endDate}&type_consult=${typeConsult}`);
+    
+    if(typeConsult !== "graphs") {
+      setDataForGraphs({});
+      setTextConsultPanel(pretty.format(res.data, { min: true }));
+    } else {
+      setDataForGraphs(res);
+    }
+    
   }
 
   useEffect(() => {
     /*
-    const ctx = chartRef.current.getContext("2d");
+    const ctx = chartRef1.current.getContext("2d");
     new Chart(ctx, {
       type: "bar",
       data: {
-        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+        labels: dataForGraphs.map((dat) => dat.date),
         datasets: [
           {
-            label: "Ventas Mensuales",
+            label: "Hashtags",
             data: [12, 19, 3, 5, 2],
+            borderWith: 1
           },
         ],
       },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
     });
     */
-  }, []);
+  }, [dataForGraphs]);
+  
+
   return (
     <>
       <nav className="navbar bg-primary">
         <div className="container">
-          <span className="navbar-brand mb-0 h1">Proyecto 2 | 202208521</span>
+          <span className="navbar-brand mb-0 h1">Proyecto 3 | 202208521</span>
         </div>
       </nav>
       <main className="container mt-4 mb-5">
@@ -39,22 +155,25 @@ export default function App() {
             <div className="col">
               <button className="btn btn-primary">Resetear datos</button>
             </div>
-            <div className="col">
+            <form onSubmit={(e) => handleMessageFileUpload(e)} className="col">
               <div className="form-group">
                 <h5>Cargar archivo de mensajes:</h5>
                 <input
+                  required
                   type="file"
                   className="custom-file-input2 btn btn-secondary"
                   id="myFileInput"
+                  onChange={(e) => onMessageFileChange(e)}
+                  accept=".xml"
                 />
               </div>
               <div>
-                <button className="btn btn-primary">Cargar</button>
-                <button className="btn btn-success" style={{marginLeft: 10}}>Obtener resumen</button>
+                <button type="submit" className="btn btn-primary">Cargar</button>
+                <button type="button" className="btn btn-success" onClick={handleMessageGet} style={{marginLeft: 10}}>Obtener resumen</button>
               </div>
-            </div>
+            </form>
 
-            <div className="col">
+            <form onSubmit={(e) => handleConfigFileUpload(e)} className="col">
               <div className="form-group">
                 <h5>Cargar archivo de configuración:</h5>
                 <input
@@ -62,23 +181,25 @@ export default function App() {
                   type="file"
                   name="load-config"
                   id="load-config"
+                  onChange={(e) => onConfigFileChange(e)}
+                  accept=".xml"
                 />
               </div>
               <div>
-                <button className="btn btn-primary">Cargar</button>
-                <button className="btn btn-success" style={{marginLeft: 10}}>Obtener resumen</button>
+                <button type="submit" className="btn btn-primary">Cargar</button>
+                <button type="button" className="btn btn-success" onClick={handleConfigGet} style={{marginLeft: 10}}>Obtener resumen</button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
 
         <div className="card mt-4">
           <div className="card-header">Peticiones</div>
           <div className="card-body row">
-            <div className="col">
+            <form onSubmit={handleSubmit} className="col">
               <div className="form-group">
                 <h6>Consultar:</h6>
-                <select className="form-control" style={{ width: "250px" }}>
+                <select value={typeConsult} onChange={(e) => setTypeConsult(e.target.value)} className="form-control" style={{ width: "250px" }}>
                   <option value="hashtags">Hashtags</option>
                   <option value="mentions">Menciones</option>
                   <option value="feelings">Sentimientos en menciones</option>
@@ -92,25 +213,32 @@ export default function App() {
                     type="date"
                     className="form-control"
                     style={{ width: "175px" }}
+                    value={startDate} 
+                    onChange={(e) => setStartDate(e.target.value)}
+                    required
                   />
                   <span className="mx-2"> - </span>
                   <input
                     type="date"
                     className="form-control"
                     style={{ width: "175px" }}
+                    value={endDate} 
+                    onChange={(e) => setEndDate(e.target.value)}
+                    required
                   />
                 </div>
               </div>
               <div className="form-group">
-                <button className="btn btn-primary">Consultar</button>
+                <button type="submit" className="btn btn-primary">Consultar</button>
               </div>
-            </div>
+            </form>
             <div className="col">
               <div>
                 <h6>Resultado</h6>
                 <textarea
                   disabled
-                  defaultValue={"Bienvenido: Realiza tu consulta"}
+                  value={textConsultPanel}
+                  onChange={setTextConsultPanel}
                   rows="14"
                   className="form-control overflow-y-auto textarea-code"
                   style={{
@@ -128,9 +256,19 @@ export default function App() {
           <div className="card-header">Gráficas</div>
           <div className="card-body">
             Selecciona "Gráficas" en Consultar para ver las gráficas.
-            { true ? <></>: (<div className="p-3 rounded" style={{backgroundColor: "#eee"}}>
-              <canvas ref={chartRef} />
-            </div>)}
+            { true ? <></>: 
+            (<>
+              <div className="p-3 rounded" style={{backgroundColor: "#eee"}}>
+                <canvas ref={chartRef1} />
+              </div>
+              <div className="p-3 rounded" style={{backgroundColor: "#eee"}}>
+                <canvas ref={chartRef2} />
+              </div>
+              <div className="p-3 rounded" style={{backgroundColor: "#eee"}}>
+                <canvas ref={chartRef3} />
+              </div>
+            </>
+            )}
           </div>
         </div>
 
@@ -139,12 +277,12 @@ export default function App() {
             <i className="fa-regular fa-circle-question"></i> Ayuda
           </h3>
           <div className="mt-3">
-            <button className="btn btn-primary" onClick={prueba}>
+            <button className="btn btn-primary" onClick={onInfoStudentButtonClicked}>
               Información de estudiante
             </button>
-            <button className="btn btn-primary mx-3">
+            <a href="https://github.com/202208521-luis-morales/IPC2_Proyecto3_202208521" target="_blank" rel="noopener noreferrer" className="btn btn-primary mx-3">
               Documentación del programa
-            </button>
+            </a>
           </div>
         </div>
       </main>
